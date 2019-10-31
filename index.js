9dd21652af4e18002d9807713be4727cddece4d3
TylerSpaulding <TylerSpaulding95@gmail.com>
2019-10-30 22:34:59 -0400

    Express set up and get all users enpoint creation

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require('express')
+
+const server = express()
+
+const db = require('./data/db.js')
+
+server.use(express.json());
+
+server.listen(4000, () => {
+    console.log('*** Server is listening on port 4000 ***');
+})
+
+//RETURNS ALL USERS
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then((user) => {
+        res.status(200).json({
+            message: user
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+        res.json({error: err})
+    })
+})
+//
